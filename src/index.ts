d8b7815af5aa15047192e93d10f5b7b0d9da91b2
samuel-bd <samuely@frisseblikken.com>
2023-11-24 13:32:01 +0700

    feat: set up SSE route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { streamSSE } from "hono/streaming";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET"],
+    allowHeaders: ["Content-Type"],
+  })
+);
 
-export default app
+app.get("/", (c) => c.text("Hello Hono!"));
+
+/**
+ * set up headers for server sent events routes
+ * must support realtime updates, so no caching
+ * Keep-Alive is for HTTP/1.1 (as HTTP/2 connections are persistent)
+ */
+app.use("/sse/*", async (c, next) => {
+  c.header("Content-Type", "text/event-stream");
+  c.header("Cache-Control", "no-cache");
+  c.header("Connection", "keep-alive");
+  await next();
+});
+
+app.get("/sse", async (c) => {
+  return streamSSE(c, async (stream) => {
+    while (true) {
+      const message = `It is ${new Date().toISOString()}`;
+      await stream.writeSSE({ data: message, event: "time-update" });
+      await stream.sleep(1000);
+    }
+  });
+});
+
+export default app;
